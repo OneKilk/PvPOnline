2c2b9ae19d8b6ab0926d6521bf128c64b40fa1c1
Tu <quangtu0210@gmail.com>
2026-02-15 19:31:17 +0700

    update1

--- server.js
+++ server.js
@@ -14,15 +14,65 @@ const io = new Server(server, {
   }
 });
 
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+let waitingPlayer = null;
+let rooms = {};
 
-  socket.on("attack", (data) => {
-    io.emit("updateBattle", data);
+io.on("connection", (socket) => {
+  console.log("Connected:", socket.id);
+
+  // Tìm trận
+  socket.on("findMatch", () => {
+    if (waitingPlayer && waitingPlayer !== socket) {
+
+      const roomId = "room_" + socket.id + "_" + waitingPlayer.id;
+
+      socket.join(roomId);
+      waitingPlayer.join(roomId);
+
+      rooms[roomId] = {
+        player1: { id: waitingPlayer.id, hp: 100 },
+        player2: { id: socket.id, hp: 100 }
+      };
+
+      io.to(roomId).emit("matchFound", {
+        roomId,
+        players: rooms[roomId]
+      });
+
+      waitingPlayer = null;
+
+    } else {
+      waitingPlayer = socket;
+      socket.emit("waiting");
+    }
+  });
+
+  // Attack
+  socket.on("attack", ({ roomId }) => {
+    const room = rooms[roomId];
+    if (!room) return;
+
+    const damage = Math.floor(Math.random() * 15) + 5;
+
+    if (room.player1.id === socket.id) {
+      room.player2.hp -= damage;
+    } else {
+      room.player1.hp -= damage;
+    }
+
+    io.to(roomId).emit("updateHP", room);
+
+    if (room.player1.hp <= 0 || room.player2.hp <= 0) {
+      io.to(roomId).emit("gameOver", room);
+      delete rooms[roomId];
+    }
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+    if (waitingPlayer === socket) {
+      waitingPlayer = null;
+    }
+    console.log("Disconnected:", socket.id);
   });
 });
 
